bcbb02ee53537d1d55d6309f5b37d46b02fbecfb
Cicek Abdullayeva <cicekabdullayeva2@gmail.com>
2021-06-30 17:26:18 -0700

    finished. v1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 $('.multiple-items').slick({
     dots: true,
     infinite: true,
-    speed: 300,
+    autoplaySpeed: 200,
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows:false,
@@ -20,23 +20,41 @@ $('.multiple-items').slick({
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 2,
-          arrows:false,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
           slidesToScroll: 1,
           arrows:false,
         }
       }
     
-    ],
-      prevArrow:"<button type='button' class='slick-prev pull-left button'><svg><use xlink:href='img/symbol-defs.svg#icon-chevron-thin-left'></use></svg></button>",
-      nextArrow:"<button type='button' class='slick-next pull-right button '><svg><use xlink:href='img/symbol-defs.svg#icon-chevron-thin-right'></use></svg></button>"
-  
+    ]
     });
-    
+
+    $('.partner-items').slick({
+      dots: true,
+      infinite: true,
+      autoplaySpeed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      arrows:false,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            arrows:false,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows:false,
+          }
+        }
+     
+      
+      ]
+      });
 });
